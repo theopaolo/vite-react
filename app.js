ec23e671869a6dc7dc30bc81e41f9360d4d77714
goeht <tpgoedert@outlook.com>
2024-05-03 15:40:41 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+`
+<article class="park">
+            <h2>${ titreDuPark }</h2>
+            <div>Carte du park ( maps )</div>
+
+            <ul class="informations">
+                <li>Animaux</li>
+                <li>Jeux</li>
+                <li>Buvette</li>
+                <li>Pétanque</li>
+            </ul>
+
+            <ul class="adresse">
+                <li>Postal code</li>
+                <li>Rue</li>
+            </ul>
+</article>
+`
